b74c510314d486479232653ddbc8c045d69bf9b6
Micokoko <mico.balina@gmail.com>
2024-01-11 19:26:50 +0800

    Added account balance for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+# app/models/user.rb
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -7,4 +8,12 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :first_name, :last_name, presence: true
 
+  before_create :set_starting_balance
+
+  private
+
+  def set_starting_balance
+    # Set starting balance only if the user is not an admin
+    self.balance = 5000 unless admin?
+  end
 end
