39583beed65a17686365f7007f5d171236e01811
Micokoko <mico.balina@gmail.com>
2024-01-18 21:00:06 +0800

    fixed api files

---
+++ config/initializers/iex.rb
@@ -0,0 +1,8 @@
+# config/initializers/iex.rb
+
+IEX::Api.configure do |config|
+    config.publishable_token = 'pk_3dbda283b9094177a492240a433bafa8'
+    config.secret_token = 'sk_47ec9c4cad8444ffb0cde92bb3fbbe2a'# Only required for certain endpoints
+    config.endpoint = 'https://cloud.iexapis.com/v1' # Defaults to IEX Cloud endpoint
+end
+  
